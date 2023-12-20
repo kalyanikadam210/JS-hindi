6d8db6a77e2739cfee0b23093b7f4b89892a7fd9
kalyani <kalyanikadam210@gmail.com>
2023-12-20 23:31:50 +0530

    create objet

--- home.js
+++ home.js
@@ -1,3 +1,10 @@
 console.log("kalyani");
 
 let  a =10;
+
+const accountId="1";
+var accountEmail="kalyanikadam@gmail.com"
+let accountCity="pune";
+
+console.log (accountEmail);
+console.log ([accountCity,accountEmail,accountId])
